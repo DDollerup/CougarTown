72c4d67a2b515814fd15d7b974a40dc29cbeaabb
PDDaniel <daniel@proper-dog.com>
2016-03-16 10:03:35 +0100

    Added Area - Admin

---
+++ CougarTown/Areas/CougarManagement/Controllers/PimpController.cs
@@ -0,0 +1,52 @@
+﻿using CougarTown.Models.BaseModels;
+using CougarTown.Models.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CougarTown.Areas.CougarManagement.Controllers
+{
+    public class PimpController : Controller
+    {
+        UserFactory userFac;
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            userFac = new UserFactory(this.HttpContext);
+            base.OnActionExecuting(filterContext);
+        }
+
+        // GET: CougarManagement/Pimp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        public ActionResult AllUsers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUserSubmit(User user, HttpPostedFileBase file)
+        {
+            // Is there a file, and is the file larger than 0 bytes
+            if (file != null && file.ContentLength > 0)
+            {
+                user.ProfileImage = file.FileName;
+                string appPath = Request.PhysicalApplicationPath;
+                file.SaveAs(appPath + @"/Content/Images/Users/" + file.FileName);
+            }
+
+
+            return RedirectToAction("AllUsers");
+        }
+    }
+}
---
+++ CougarTown/Areas/CougarManagement/CougarManagementAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CougarTown.Areas.CougarManagement
+{
+    public class CougarManagementAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CougarManagement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CougarManagement_default",
+                "CougarManagement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
